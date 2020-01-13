0ccab4ff63deb90653f18c02f191c5c5326a9df7
licm34 <50062132+licm34@users.noreply.github.com>
2020-01-13 20:12:11 +0900

    2-winter

--- RUBY/opentutorials/Loop.rb
+++ RUBY/opentutorials/Loop.rb
@@ -12,3 +12,22 @@ while j<10 do
   print(j*3  )
   j=j+1
 end
+print("\n")
+
+
+alpabet = ['A', 'B', 'C', 'D']
+for c in alpabet
+    puts(c)
+end
+
+
+puts("ENTER ID")
+input_id=gets.chomp()
+ids=['a', 'b', 'c']
+for id in ids do
+    if id == input_id
+        puts("hello, "+id)
+        exit
+      end
+    end
+print("who r u?")
